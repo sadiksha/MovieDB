42cc57321b45d7143230fe060060f562b060665b
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-06-28 21:30:42 +0200

    Adds new and create actions to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,20 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
   end
 end
