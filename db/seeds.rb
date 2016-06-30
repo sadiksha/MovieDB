7d7ad6ebf5c061670a10a5ab2f5a75e966c27e33
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-06-30 21:09:12 +0200

    Adds seed data for creating category.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+["Action", "Drama", "Horror", "Comedy"].each do |category_name|
+  Category.first_or_create(name: category_name)
+end
