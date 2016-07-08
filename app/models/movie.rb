f6770a779ed082fc87a84918d35983846234a749
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-07-08 20:13:14 +0200

    Adds association between movie, rating and user. Adds test for association between rating and movie.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
   # has two attributes title and description
   belongs_to :category
+  has_many :ratings
 
   accepts_nested_attributes_for :category
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -2,8 +2,16 @@ require "rails_helper"
 
 describe Rating, :type => :model do
   it "has attributes score" do
-    rating = Rating.new(score: 3)
-    rating.save
-    expect(rating.score).to eq(3)
+    rating = FactoryGirl.create(:rating)
+    expect(rating.score).to eq(1)
+  end
+
+  describe "belongs to" do
+    it "movie" do
+      rating = FactoryGirl.create(:rating)
+      movie = rating.movie
+      expect(movie.title).to eq("My First Movie")
+      expect(movie.description).to eq("This is my first description.")
+    end
   end
 end
