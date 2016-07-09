5a05f50910924406f35eed547f53f99ca9c9e487
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-07-09 14:43:47 +0200

    Validates duplicate ratings for same user and movie with tests.

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -29,4 +29,30 @@ describe Rating, :type => :model do
       result = Rating.exists_for_user_and_movie?(movie, user)
     end
   end
+
+  it "cannot have same user and movie multiple times" do
+    rating = create(:rating)
+    user = rating.user
+    movie = rating.movie
+    new_rating = Rating.new(user: user, movie: movie)
+    new_rating.save
+    error_message = new_rating.errors.messages.values.flatten.first
+    expect(error_message).to eq("can't be created twice for same user and movie.")
+  end
+
+  it "can be different for different users for single movie" do
+    rating = create(:rating)
+    second_user = create(:user, email: "testuser@test.com")
+    second_rating = FactoryGirl.create(:rating, user: second_user, movie: rating.movie)
+    error_messages = []
+    expect(error_messages).to be_empty
+  end
+
+  it "can be different for different movies and same user" do
+    rating = create(:rating)
+    movie = create(:movie, title: "Second movie")
+    rating = FactoryGirl.create(:rating, user: rating.user, movie: movie)
+    error_messages = []
+    expect(error_messages).to be_empty
+  end
 end
