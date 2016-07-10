d924b4ff35589c3c79423b7bad3076af7b580eed
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-07-10 14:38:34 +0200

    Searches the movie and category via ajax.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,8 @@ class MoviesController < ApplicationController
 
   before_filter :authenticate_user!, except: [:index, :show]
   def index
-    @movies = Movie.all.paginate(:page => params[:page], :per_page => 10)
+    relevant_movies = Movie.search(params[:category], params[:movie])
+    @movies = relevant_movies.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
