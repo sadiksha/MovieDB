6bed97023c7bd7168591f8e5a64de6d95f0a51e8
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-07-11 23:08:03 +0200

    Displays radio button via ember js.

--- app/assets/javascripts/components/movie-rating.js
+++ app/assets/javascripts/components/movie-rating.js
@@ -1,2 +1,7 @@
 MovieDB.MovieRatingComponent = Ember.Component.extend({
+  rating: Ember.computed.alias('targetObject.content'),
+  score: 0,
+  click: function(event){
+    this.set('score', $(event.target).val());
+    }
 })
