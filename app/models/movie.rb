27e0d79fdb614813dc5d48879b1af94a79dabc64
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2016-07-12 20:30:08 +0200

    Adds dependent destroy for rating on movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   # has two attributes title and description
   belongs_to :category
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :users, through: :ratings
 
   accepts_nested_attributes_for :category
